47a001d4458d348f8e70a7f4b7a1614769026197
EVGENY M <168018528+rjohny55@users.noreply.github.com>
2025-03-18 22:11:03 +0600

    Create Controls.js

---
+++ components/Controls.js
@@ -0,0 +1,9 @@
+export default function Controls() {
+  return (
+    <div id="controls" style={{ display: 'none' }}>
+      <button id="leftButton">←</button>
+      <button id="rotateButton">↻</button>
+      <button id="rightButton">→</button>
+    </div>
+  );
+}
