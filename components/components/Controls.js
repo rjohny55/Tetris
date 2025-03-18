696f4f4366a4d092a64a338f57f2287781b65499
EVGENY M <168018528+rjohny55@users.noreply.github.com>
2025-03-18 22:11:15 +0600

    Delete components/components/Controls.js

--- components/components/Controls.js
+++
@@ -1,9 +0,0 @@
-export default function Controls() {
-  return (
-    <div id="controls" style={{ display: 'none' }}>
-      <button id="leftButton">←</button>
-      <button id="rotateButton">↻</button>
-      <button id="rightButton">→</button>
-    </div>
-  );
-}
