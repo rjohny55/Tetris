8806ed9185daa436cc38fadc0723732b26ebb870
EVGENY M <168018528+rjohny55@users.noreply.github.com>
2025-04-01 22:15:46 +0600

    Delete components directory

--- components/ScoreBoard.js
+++
@@ -1,8 +0,0 @@
-export default function ScoreBoard({ score, level }) {
-  return (
-    <div id="scoreBoard" style={{ display: 'none' }}>
-      Score: <span id="score">{score}</span><br />
-      Level: <span id="level">{level}</span>
-    </div>
-  );
-}
